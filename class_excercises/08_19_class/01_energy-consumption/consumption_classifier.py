1edd6786a4fc0e6a45e724fe0f8f1c580c7e272a
lucamaci0 <luca.maci@it.ey.com>
2025-08-19 10:49:13 +0200

    feature(energy_classifier): computation of daily/weekly/monthly/yearly averages done

--- class_excercises/08_19_class/01_energy-consumption/consumption_classifier.py
+++ class_excercises/08_19_class/01_energy-consumption/consumption_classifier.py
@@ -7,3 +7,31 @@ dataset_dir = os.path.join(datasets_dir,dataset)
 
 df = pd.read_csv(dataset_dir, parse_dates=["Datetime"])
 print(df.head())
+
+df["hour"] = df["Datetime"].dt.hour
+# df["dayofweek"] = df["Datetime"].dt.dayofweek
+df["dayofyear"] = df["Datetime"].dt.dayofyear
+df["week"] = df["Datetime"].dt.isocalendar().week
+df["month"] = df["Datetime"].dt.month
+df["year"] = df["Datetime"].dt.year
+
+df["daily_avg"] = (
+    df.groupby([df["year"], df["dayofyear"]])["AEP_MW"]
+      .transform("mean")
+)
+df["weekly_avg"] = (
+    df.groupby([df["year"], df["week"]])["AEP_MW"]
+      .transform("mean")
+)
+df["monthly_avg"] = (
+    df.groupby([df["year"], df["month"]])["AEP_MW"]
+      .transform("mean")
+)
+df["yearly_avg"] = (
+    df.groupby("year")["AEP_MW"]
+      .transform("mean")
+)
+
+
+print(df.head(50))
+
