f5a50319cb66a7d7d48ba3de2e2fc55254f7fbe6
lucamaci0 <luca.maci@it.ey.com>
2025-08-19 11:32:33 +0200

    feat(energy_classifier): added cute graph. TODO: fix plt.show() to prevent it from blocking code.

--- class_excercises/08_19_class/01_energy-consumption/consumption_classifier.py
+++ class_excercises/08_19_class/01_energy-consumption/consumption_classifier.py
@@ -1,12 +1,17 @@
 import os
 import pandas as pd
+import matplotlib.pyplot as plt
+
 
 datasets_dir = "other/Archivio Datasets/02_Lesson"
 dataset = "AEP_hourly.csv"
 dataset_dir = os.path.join(datasets_dir,dataset)
 
 df = pd.read_csv(dataset_dir, parse_dates=["Datetime"])
-print(df.head())
+df = df.sort_values("Datetime").reset_index(drop=True)
+
+
+# Derive better calendar features from the Datetime column
 
 df["hour"] = df["Datetime"].dt.hour
 # df["dayofweek"] = df["Datetime"].dt.dayofweek
@@ -32,10 +37,29 @@ df["yearly_avg"] = (
       .transform("mean")
 )
 
+
+# Creation of target variables
+
 df["target_daily"] = (df["AEP_MW"] > df["daily_avg"]).astype(int)
 df["target_weekly"] = (df["AEP_MW"] > df["weekly_avg"]).astype(int)
 df["target_monthly"] = (df["AEP_MW"] > df["monthly_avg"]).astype(int)
 df["target_yearly"] = (df["AEP_MW"] > df["yearly_avg"]).astype(int)
 
-print(df.head(50))
 
+ # Flavour plot
+
+df_filtered = df[df["Datetime"] >= "2018-01-01"]
+
+plt.figure(figsize=(15,6))
+
+# Blue line: actual consumption
+plt.plot(df_filtered["Datetime"], df_filtered["AEP_MW"], color="blue", label="Hourly Consumption", linewidth=0.4)
+
+# Red line: yearly average (step-like constant within each year)
+plt.plot(df_filtered["Datetime"], df_filtered["monthly_avg"], color="red", label="Monthly Average", linewidth=1)
+
+plt.xlabel("Datetime")
+plt.ylabel("Consumption (MW)")
+plt.title("Hourly Consumption vs Monthly Average past 2018")
+plt.legend()
+plt.show() # This is blocking until the figure is manually closed. Why?
