ba3a9a31273a9baefeb35b7372a8ce254dace490
lucamaci0 <luca.maci@it.ey.com>
2025-08-21 12:59:32 +0200

    feature(semantic_analysis): implemented displacy's tree graph of the sentence's semantic structure

--- class_excercises/08_21_class/tokenizer.py
+++ class_excercises/08_21_class/tokenizer.py
@@ -1,5 +1,12 @@
-import spacy
+import os
 import tiktoken
+import spacy
+from spacy import displacy
+from pathlib import Path
+
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+PLOTS_DIR = os.path.join(CURRENT_DIR, "plots")
+
 
 # Choose the encoding for a model (e.g., GPT-4o, GPT-3.5-turbo)
 encoding = tiktoken.encoding_for_model("gpt-4o")
@@ -13,6 +20,7 @@ text = "Ciao ChatGPT! Scrivi una poesia su un gatto che cammina sul tetto."
 doc = nlp(text)
 for token in doc:
     print(f"Token: {token.text:12} | Lemma: {token.lemma_:12} | POS: {token.pos_:10} | Dipendenza: {token.dep_:10} | Head: {token.head.text}")
+print("\n")
 
 # Token Analysis
 tokens = encoding.encode(text)
@@ -20,3 +28,9 @@ print("Tokens:", tokens)
 print("Number of tokens:", len(tokens))
 decoded = encoding.decode(tokens)
 print("Decoded:", decoded)
+
+# Tree-structure of the sentence's semantic analysis
+svg = displacy.render(doc, style="dep", options={"compact": True, "distance": 120})
+output_path_svg = Path(f"{PLOTS_DIR}/dep_tree.svg")
+output_path_svg.open("w", encoding="utf-8").write(svg)
+print("Saved semantic analysis of the sentence in plots/dep_tree.svg")
